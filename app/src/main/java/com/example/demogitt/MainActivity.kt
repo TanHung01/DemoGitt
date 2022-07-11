e479796f978ea76b9cbc2d56308635758828af2a
TanHung01 <buitanhung0401@gmail.com>
2022-07-11 10:34:55 +0700

    file fist branch

--- app/src/main/java/com/example/demogitt/MainActivity.kt
+++ app/src/main/java/com/example/demogitt/MainActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 
 //pass  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
 
+//////
+/////       new code ne
+////
+///
+//
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
